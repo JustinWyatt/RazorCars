3d59d5293498bdefc434cbccbd630863ea10b3dd
Josh Hill <joshuahill84@gmail.com>
2015-12-21 11:59:03 -0600

    Initial Commit

---
+++ RazorCars.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RazorCars.Web.Startup))]
+namespace RazorCars.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
